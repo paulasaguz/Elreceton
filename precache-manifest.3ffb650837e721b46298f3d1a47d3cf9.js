57cba327512c2e7de52fbf6a8a57c4d107f75c08
Paula Sanchez <Paula@MacBook-Pro-de-Leonidas.local>
2018-12-19 15:36:07 -0600

    Updates

---
+++ precache-manifest.3ffb650837e721b46298f3d1a47d3cf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49dde9a43e5b82bd6fb3",
+    "url": "/Elreceton/static/js/runtime~main.49dde9a4.js"
+  },
+  {
+    "revision": "745a822e0cd4493da978",
+    "url": "/Elreceton/static/js/main.745a822e.chunk.js"
+  },
+  {
+    "revision": "6a93712b6c5a96a99448",
+    "url": "/Elreceton/static/js/1.6a93712b.chunk.js"
+  },
+  {
+    "revision": "745a822e0cd4493da978",
+    "url": "/Elreceton/static/css/main.eacb8cfc.chunk.css"
+  },
+  {
+    "revision": "6a93712b6c5a96a99448",
+    "url": "/Elreceton/static/css/1.83e0922a.chunk.css"
+  },
+  {
+    "revision": "6b23545e3e0207e810fe201192adf70d",
+    "url": "/Elreceton/index.html"
+  }
+];
